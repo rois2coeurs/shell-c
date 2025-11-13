99f4d08d93b2f1a3d0edf0a7f21a7b12d3fbab57
rois2coeurs <rois2coeurs@gmail.com>
2025-11-13 12:00:20 +0100

    echo command

--- src/main.c
+++ src/main.c
@@ -4,15 +4,28 @@
 
 #define BUFFER_SIZE 128
 
-void handle_cmd(char *cmd) {
-  char *token = strtok(cmd, " ");
+void exit_cmd(char *exit_status) {
+  if (exit_status[0] == '0')
+    exit(EXIT_SUCCESS);
+  else
+    exit(EXIT_FAILURE);
+}
+
+void echo_cmd(char *message) { printf("%s\n", message); }
+
+void handle_cmd(char *cmd, int cmd_len) {
+  char cmd_cpy[cmd_len];
+  memcpy(cmd_cpy, cmd, cmd_len);
+  char *token = strtok(cmd_cpy, " ");
   // printf("token='%s'\n", token);
   if (strcmp(token, "exit") == 0) {
     token = strtok(NULL, " ");
-    if (token[0] == '0')
-      exit(EXIT_SUCCESS);
-    else
-      exit(EXIT_FAILURE);
+    exit_cmd(token);
+    return;
+  }
+  if (strcmp(token, "echo") == 0) {
+    echo_cmd(cmd + 5);
+    return;
   }
 
   printf("%s: command not found\n", cmd);
@@ -28,7 +41,7 @@ int main(int argc, char *argv[]) {
     char *line = NULL;
     ssize_t bytes_read = getline(&line, &len, stdin);
     line[bytes_read - 1] = '\0';
-    handle_cmd(line);
+    handle_cmd(line, bytes_read);
   }
 
   return 0;
