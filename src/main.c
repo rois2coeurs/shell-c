995a9b00230e76c224d225137b33f041095eb0fc
rois2coeurs <rois2coeurs@gmail.com>
2025-11-13 16:12:22 +0100

    execute external files

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 128
 #define BUILT_IN_CMD                                                           \
@@ -15,10 +18,8 @@ int is_in_path(char *cmd, char *res_path) {
     return 0;
   char paths[strlen(raw_paths) + 1];
   strcpy(paths, raw_paths);
-  // printf("paths='%s'\n", paths);
   char *path = strtok(paths, ":");
   while (path != NULL) {
-    // printf("path='%s'\n", path);
     struct dirent *de;
     DIR *dr = opendir(path);
     if (dr != NULL) {
@@ -30,9 +31,12 @@ int is_in_path(char *cmd, char *res_path) {
         strcat(res_buffer, "/");
         strcat(res_buffer, de->d_name);
         struct stat fs;
-        if (stat(res_buffer, &fs) < 0) continue;
-        if (!S_ISREG(fs.st_mode)) continue;
-        if (!(fs.st_mode & S_IXUSR)) continue;
+        if (stat(res_buffer, &fs) < 0)
+          continue;
+        if (!S_ISREG(fs.st_mode))
+          continue;
+        if (!(fs.st_mode & S_IXUSR))
+          continue;
         strcpy(res_path, res_buffer);
         return 1;
       }
@@ -67,11 +71,30 @@ void type_cmd(char *cmd) {
   printf("%s: not found\n", cmd);
 }
 
+void execute_external_file(char *executable_name, char *exe_path,
+                           char *raw_args) {
+  pid_t pid = fork();
+  if (pid == 0) {
+    char *token = strtok(raw_args, " ");
+    char *args[64];
+    args[0] = executable_name;
+    int args_len = 1;
+    while (token != NULL) {
+      args[args_len++] = token;
+      token = strtok(NULL, " ");
+    }
+    args[args_len] = NULL;
+    execv(exe_path, args);
+    exit(127);
+  } else {
+    waitpid(pid, 0, 0);
+  }
+}
+
 void handle_cmd(char *cmd, int cmd_len) {
   char cmd_cpy[cmd_len];
   memcpy(cmd_cpy, cmd, cmd_len);
   char *token = strtok(cmd_cpy, " ");
-  // printf("token='%s'\n", token);
   if (strcmp(token, "exit") == 0) {
     token = strtok(NULL, " ");
     exit_cmd(token);
@@ -83,6 +106,11 @@ void handle_cmd(char *cmd, int cmd_len) {
   }
   if (strcmp(token, "type") == 0) {
     type_cmd(cmd + 5);
+    return;
+  }
+  char path_buffer[1024];
+  if (is_in_path(token, path_buffer)) {
+    execute_external_file(token, path_buffer, cmd + strlen(token) + 1);
     return;
   }
 
