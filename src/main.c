801d05ac4380e5b66c093a465218a10786ae0c9f
rois2coeurs <rois2coeurs@gmail.com>
2025-11-13 11:02:57 +0100

    handle exit cmd

--- src/main.c
+++ src/main.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void handle_cmd(char *cmd) {
+  char *token = strtok(cmd, " ");
+  if (strcmp(token, "exit") == 0) {
+    token = strtok(NULL, " ");
+    if (token[0] == '0')
+      exit(EXIT_SUCCESS);
+    else
+      exit(EXIT_FAILURE);
+  }
+
+  printf("%s: command not found\n", cmd);
+}
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   char buffer[128];
 
-  while(1) {
-      printf("$ ");
-      scanf("%s", buffer);
-      printf("%s: command not found\n", buffer);
+  while (1) {
+    printf("$ ");
+    scanf("%[^\n]s", buffer);
+    handle_cmd(buffer);
+    memset(buffer, '\0', 128);
   }
 
   return 0;
